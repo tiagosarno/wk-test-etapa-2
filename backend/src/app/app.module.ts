c9e573c244a814c9096a7f8978ef3abf2fb69078
Senior Developer <eng.tiagorocha@gmail.com>
2025-12-03 19:42:27 -0300

    feat: (app) - App module implementations

---
+++ backend/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from 'src/users/users.module';
+import { LoggerMiddleware } from 'src/common/middlewares/logger.middleware';
+import { AuthModule } from 'src/auth/auth.module';
+import { MessageModule } from 'src/messages/message.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna ConfigService disponível em toda a aplicação
+      envFilePath: process.env.NODE_ENV
+        ? `.env.${process.env.NODE_ENV}`
+        : '.env',
+    }),
+    PrismaModule,
+    UsersModule,
+    MessageModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
